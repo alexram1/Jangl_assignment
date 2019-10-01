8046b128836aee05ecd1985e7e03f501d6889778
Alex Ram <aram1@uvm.edu>
2019-10-01 18:41:27 -0400

    completed logic to replace phone numbers in correct format

--- insertTrackedNumber.js
+++ insertTrackedNumber.js
@@ -9,8 +9,41 @@ function updateNumber(current_number){
     //var urlParams = new URLSearchParams(window.location.search); // more dynamic but uses a framework
     //var new_number = urlParams.get('number')
     
-    var number = new_number + current_number;
-    return number;
+    //split the new number and remove the 1 at the beggining if it contains it
+    var new_num_array = new_number.split('');
+    while (new_num_array.length > 10) {
+	new_num_array.shift(); 
+    }
+    
+    //find the indicies we want to subsitute a new number into
+    var regex = /[0-9]/g, result, indices = [];
+    while ( (result = regex.exec(current_number)) ) {
+        indices.push(result.index);
+    }
+    while (indices.length > 10) {
+	indices.shift(); 
+    }
+    
+    //build formated number string
+    var formated_number = '';
+    //default formatting
+    if (indices.length < 10) {
+	return '('.concat(new_num_array[0],new_num_array[1],new_num_array[2],') ',
+        new_num_array[3],new_num_array[4],new_num_array[5],'-',new_num_array[6],
+        new_num_array[7],new_num_array[8],new_num_array[9]);
+    }
+    //non-default formatting
+    for (var i = 0; i < current_number.length; i++) {
+        if (indices.includes(i)) {
+            formated_number = formated_number.concat(new_num_array[0]);
+            new_num_array.shift();
+        }
+        else {
+            formated_number = formated_number.concat(current_number.charAt(i));
+        }
+    }
+    
+    return formated_number;
 }
 
 (function(){ // javascript closure
@@ -25,7 +58,7 @@ function updateNumber(current_number){
     // I don't know if that's intended, but any easy solution would be to add the class name to the other link.
     var tracked_number_links = document.getElementsByClassName('tracked_number_link');
     for (var i = 0; i < tracked_number_links.length; i++) {
-        var current_number = tracked_number_links[i];
+        var current_number = tracked_number_links[i].href;
         document.getElementsByClassName('tracked_number_link')[i].href = updateNumber(current_number);
     }
 })();
